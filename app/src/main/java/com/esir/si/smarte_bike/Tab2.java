09cb86c51b0a3d456c91ea9881667bb014e787db
Amira <amiraromdhane22@gmail.com>
2015-02-10 18:03:15 +0100

    implement google maps

--- app/src/main/java/com/esir/si/smarte_bike/Tab2.java
+++ app/src/main/java/com/esir/si/smarte_bike/Tab2.java
@@ -1,17 +1,118 @@
 package com.esir.si.smarte_bike;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 
 
-public class Tab2 extends Fragment {
-    @Override
+public class Tab2 extends Fragment implements LocationListener{
+    private LocationManager locationManager;
+    private GoogleMap gMap;
+    private Marker marker;
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        //gMap = ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
+        //marker = gMap.addMarker(new MarkerOptions().title("Vous êtes ici").position(new LatLng(0, 0)));
         View v = inflater.inflate(R.layout.tab_2, container, false);
         return v;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //Obtention de la référence du service
+        locationManager = (LocationManager) this.getActivity().getSystemService(getActivity().LOCATION_SERVICE);
+
+        //Si le GPS est disponible, on s'y abonne
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            abonnementGPS();
+        }
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        //On appelle la méthode pour se désabonner
+        desabonnementGPS();
+    }
+
+    /**
+     * Méthode permettant de s'abonner à la localisation par GPS.
+     */
+    public void abonnementGPS() {
+        //On s'abonne
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, this);
+    }
+
+    /**
+     * Méthode permettant de se désabonner de la localisation par GPS.
+     */
+    public void desabonnementGPS() {
+        //Si le GPS est disponible, on s'y abonne
+        locationManager.removeUpdates(this);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onLocationChanged(final Location location) {
+        //On affiche dans un Toat la nouvelle Localisation
+        final StringBuilder msg = new StringBuilder("lat : ");
+        msg.append(location.getLatitude());
+        msg.append( "; lng : ");
+        msg.append(location.getLongitude());
+
+        Toast.makeText(this.getActivity(), msg.toString(), Toast.LENGTH_SHORT).show();
+
+        //Mise à jour des coordonnées
+        final LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+        marker.setPosition(latLng);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onProviderDisabled(final String provider) {
+        //Si le GPS est désactivé on se désabonne
+        if("gps".equals(provider)) {
+            desabonnementGPS();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onProviderEnabled(final String provider) {
+        //Si le GPS est activé on s'abonne
+        if("gps".equals(provider)) {
+            abonnementGPS();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onStatusChanged(final String provider, final int status, final Bundle extras) { }
 }
