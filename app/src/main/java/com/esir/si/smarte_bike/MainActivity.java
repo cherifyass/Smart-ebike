204d717a9bc48fb3fede14184ad4c90343f14ee9
Quentin Le Goff <lg.quentin@gmail.Com>
2015-03-02 17:44:12 +0100

    remise à zéro de la main activity pour accueillir la nouvelle interface

--- app/src/main/java/com/esir/si/smarte_bike/MainActivity.java
+++ app/src/main/java/com/esir/si/smarte_bike/MainActivity.java
@@ -3,63 +3,24 @@ package com.esir.si.smarte_bike;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import tabs.SlidingTabLayout;
 
 
 
 
 public class MainActivity extends ActionBarActivity {
 
-    // Declaring Views and Variables
-    private Toolbar mToolbar;
-    private ViewPager mPager;
-    private ViewPagerAdapter mAdapter;
-    private SlidingTabLayout mTabs;
-    private CharSequence mTitles[];
-    private int Numboftabs = 2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Creating The Toolbar and setting it as the Toolbar for the activity
-        mToolbar = (Toolbar) findViewById(R.id.tool_bar);
-        setSupportActionBar(mToolbar);
-
-        // Getting title strings resources
-        mTitles = getResources().getStringArray(R.array.tabs);
-
-        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles of the Tabs and Number Of Tabs.
-        mAdapter =  new ViewPagerAdapter(getSupportFragmentManager(),mTitles,Numboftabs);
-
-        // Assigning ViewPager View and setting the adapter
-        mPager = (ViewPager) findViewById(R.id.pager);
-        mPager.setAdapter(mAdapter);
-
-        // Assigning the Sliding Tab Layout View
-        mTabs = (SlidingTabLayout) findViewById(R.id.tabs);
-        mTabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
-
-        // Setting Custom Color for the Scroll bar indicator of the Tab View
-        mTabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
-            @Override
-            public int getIndicatorColor(int position) {
-                return getResources().getColor(R.color.tabsScrollColor);
-            }
-        });
-
-        // Setting the ViewPager For the SlidingTabsLayout
-        mTabs.setViewPager(mPager);
-
 
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -76,19 +37,11 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        switch(item.getItemId()){
-            case R.id.action_settings:
-                // handle action
-                return true;
-            case R.id.action_about:
-                // handle action
-                return true;
-            case R.id.action_navigation:
-                Intent intent = new Intent(MainActivity.this, NavigationActivity.class);
-                startActivity(intent);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        if (id == R.id.action_settings) {
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
     }
+
 }
---
+++ old/mainactivity.java
@@ -0,0 +1,94 @@
+package com.esir.si.smarte_bike;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import tabs.SlidingTabLayout;
+
+
+
+
+public class MainActivity extends ActionBarActivity {
+
+    // Declaring Views and Variables
+    private Toolbar mToolbar;
+    private ViewPager mPager;
+    private ViewPagerAdapter mAdapter;
+    private SlidingTabLayout mTabs;
+    private CharSequence mTitles[];
+    private int Numboftabs = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Creating The Toolbar and setting it as the Toolbar for the activity
+        mToolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(mToolbar);
+
+        // Getting title strings resources
+        mTitles = getResources().getStringArray(R.array.tabs);
+
+        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles of the Tabs and Number Of Tabs.
+        mAdapter =  new ViewPagerAdapter(getSupportFragmentManager(),mTitles,Numboftabs);
+
+        // Assigning ViewPager View and setting the adapter
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
+        // Assigning the Sliding Tab Layout View
+        mTabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        mTabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        mTabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabsScrollColor);
+            }
+        });
+
+        // Setting the ViewPager For the SlidingTabsLayout
+        mTabs.setViewPager(mPager);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch(item.getItemId()){
+            case R.id.action_settings:
+                // handle action
+                return true;
+            case R.id.action_about:
+                // handle action
+                return true;
+            case R.id.action_navigation:
+                Intent intent = new Intent(MainActivity.this, NavigationActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
