51a90fee28cecbf210dd2643d7301ba136c29267
Quentin Le Goff <lg.quentin@gmail.Com>
2015-03-02 13:14:02 +0100

    Réorganisation du fichier strings.xml et modification interface "Mes données"

--- app/src/main/java/com/esir/si/smarte_bike/donnees.java
+++ app/src/main/java/com/esir/si/smarte_bike/donnees.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class donnees extends ActionBarActivity {
+public class Donnees extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
