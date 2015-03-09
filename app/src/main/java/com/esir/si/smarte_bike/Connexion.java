75e37f1a5dcffb2f6a47eb2288c7a67892de88d9
Quentin Le Goff <lg.quentin@gmail.Com>
2015-03-09 11:25:32 +0100

    Ajout de l'interface de connexion et implémentation du Bluetooth

--- app/src/main/java/com/esir/si/smarte_bike/Connexion.java
+++ app/src/main/java/com/esir/si/smarte_bike/Connexion.java
@@ -1,17 +1,89 @@
 package com.esir.si.smarte_bike;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
 
 
 public class Connexion extends ActionBarActivity {
+
+    public boolean connecte = false;
+    private TextView etat = null;
+    private TextView message = null;
+
+    public BluetoothDevice bdevice = null;
+    public BluetoothSocket bsocket = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+
+        etat = (TextView) findViewById(R.id.etat);
+        if (connecte)
+            etat.setText("Connecté");
+
+        else
+            etat.setText("Non connecté");
+
+
+        message = (TextView) findViewById(R.id.message);
+
+
+    }
+
+    public void seConnecter(View view) {
+        BluetoothAdapter blueAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (blueAdapter == null) {
+            message.setText("Votre téléphone ne dispose pas de la technologie Bluetooth,hahahaha t'es nul !!!!");
+        }
+
+        if (!blueAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+        if (blueAdapter.isEnabled()) {
+            message.setText("Bluetooth activé ! Youpi ;)");
+        }
+
+        Set<BluetoothDevice> pairedDevices = blueAdapter.getBondedDevices();
+
+        blueAdapter.startDiscovery();
+        blueAdapter.cancelDiscovery();
+
+        // On crée un BroadcastReceiver pour ACTION_FOUND
+        final BroadcastReceiver receiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                ArrayAdapter mArrayAdapter =null;
+                // Quand la recherche trouve un terminal
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    // On récupère l'object BluetoothDevice depuis l'Intent
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    // On ajoute le nom et l'adresse du périphérique dans un ArrayAdapter (par exemple pour l'afficher dans une ListView)
+                    mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                }
+            }
+        };
+        // Inscrire le BroadcastReceiver
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter); // N'oubliez pas de le désinscrire lors du OnDestroy() !
+
     }
 
 
--- app/src/main/java/com/esir/si/smarte_bike/MainActivity.java
+++ app/src/main/java/com/esir/si/smarte_bike/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.EditText;
 
 
 import java.util.List;
