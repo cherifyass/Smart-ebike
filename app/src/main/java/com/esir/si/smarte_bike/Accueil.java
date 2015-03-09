403b355ed2f025677c5cfc5b070257eb3fbdb150
othmansanhaji <othmansanhaji@gmail.com>
2015-03-09 11:18:49 +0100

    Animation de connexion

--- app/src/main/java/com/esir/si/smarte_bike/Accueil.java
+++ app/src/main/java/com/esir/si/smarte_bike/Accueil.java
@@ -1,19 +1,32 @@
 package com.esir.si.smarte_bike;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-/**
- * Created by Quentin on 03/03/2015.
- */
-public class Accueil extends Fragment {
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+        import android.view.animation.Animation;
+        import android.view.animation.AnimationUtils;
+        import android.widget.Button;
+        import android.widget.TextView;
+public class Accueil extends Activity implements OnClickListener {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.accueil, container, false);
+    Animation animationconnect;
+    Button monBouton;
+    TextView monTextView;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        monTextView = (TextView) findViewById(R.id.button);
+        monBouton = (Button) findViewById(R.id.button);
+        monBouton.setOnClickListener(this);
+        animationconnect = AnimationUtils.loadAnimation(this, R.anim.connectanim);
+    }
+    public void onClick(View v) {
+        if(v.getId() == R.id.button){
+            monTextView.startAnimation(animationconnect);
+
+        }
     }
 }
