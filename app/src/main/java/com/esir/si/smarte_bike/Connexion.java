03d822257af955abcb904a97de438e4657cbc6ef
Quentin Le Goff <lg.quentin@gmail.com>
2015-05-04 00:39:25 +0200

    Communication bluetooth - verouillage, deverouillage, light

--- app/src/main/java/com/esir/si/smarte_bike/Connexion.java
+++ app/src/main/java/com/esir/si/smarte_bike/Connexion.java
@@ -29,38 +29,24 @@ import java.util.UUID;
 public class Connexion extends ActionBarActivity {
 
     public boolean connecte = false;
+
     private TextView etat = null;
     private TextView message = null;
 
-    private static int REQUEST_CODE_ENABLE_BLUETOOTH = 0;
-    private static final long SCAN_PERIOD = 10000;
 
-    final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-    //BluetoothAdapter mBluetoothAdapter = bluetoothManager.getAdapter();
-
-    private boolean mScanning;
-    private Handler mHandler;
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    BluetoothDevice mmDevice = null;
+    private UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
 
-        etat = (TextView) findViewById(R.id.etat);
-
-
-        if (connecte)
-            etat.setText("Connecté au vélo");
-
-        else
-            etat.setText("Non connecté au vélo");
-
-        message = (TextView) findViewById(R.id.message);
 
         //Est ce que le téléphone a le bluetooth ?
         bluetoothExiste();
-
-
+        estConnecte();
 
     }
 
@@ -69,25 +55,47 @@ public class Connexion extends ActionBarActivity {
     //Recherche de la techno
     public void bluetoothExiste(){
         if (mBluetoothAdapter == null)
-            Toast.makeText(this, "Pas de Bluetooth",
+            Toast.makeText(this, "Votre téléphone n'a pas le Bluetooth, vous ne pouvez pas utiliser l'application au maximum de ses fonctionnalités.",
                     Toast.LENGTH_SHORT).show();
         else
-            Toast.makeText(this, "Téléphone avec Bluetooth, ça commence bien",
+            Toast.makeText(this, "Le téléphone dispose du Bluetooth, veuillez le connecter au vélo.",
                     Toast.LENGTH_SHORT).show();
     }
 
+    public void estConnecte(){
+
+        etat = (TextView) findViewById(R.id.etat);
+        connecte = mBluetoothAdapter.isEnabled();
+
+        if (connecte)
+            etat.setText("Connecté au vélo");
+        else
+            etat.setText("Non connecté au vélo");
+
+    }
 
     public void seConnecter(View view){
 
-        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "En cours de connexion", Toast.LENGTH_SHORT).show();
 
         //Activation du bluetooth
-        if (!mBluetoothAdapter.isEnabled()) {
-            Intent enableBlueTooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBlueTooth, REQUEST_CODE_ENABLE_BLUETOOTH);
+        if(!mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+
+            Set<BluetoothDevice> setpairedDevices = BluetoothAdapter.getDefaultAdapter().getBondedDevices();
+            BluetoothDevice[] pairedDevices = (BluetoothDevice[]) setpairedDevices.toArray(new BluetoothDevice[setpairedDevices.size()]);
+
+            // On parcours la liste pour trouver notre module bluetooth
+            for(int i=0;i<pairedDevices.length;i++)
+            {
+                // On teste si ce périphérique contient le nom du module bluetooth connecté au microcontrôleur
+                if(pairedDevices[i].getName().contains("raspberrypi")) {
+                    mmDevice = pairedDevices[i];
+                }
+            }
         }
-
-
     }
 
 
@@ -95,16 +103,19 @@ public class Connexion extends ActionBarActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode != REQUEST_CODE_ENABLE_BLUETOOTH)
+        if (requestCode != 0)
             return;
         if (resultCode == RESULT_OK) {
             // L'utilisation a activé le bluetooth
             Toast.makeText(this, "Bluetooth activé", Toast.LENGTH_SHORT).show();
+
         } else {
             // L'utilisation n'a pas activé le bluetooth
             Toast.makeText(this, "Erreur durant l'activation du bluetooth", Toast.LENGTH_SHORT).show();
 
         }
+
+        estConnecte();
     }
 
     @Override
@@ -132,5 +143,10 @@ public class Connexion extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void RPi(View view){
+        Intent intent = new Intent(this, RPi.class);
+        startActivity(intent);
     }
 }
