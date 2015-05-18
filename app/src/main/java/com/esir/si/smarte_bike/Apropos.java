6f765b603c1f5cd501f323086e38f97d8dd85d6b
Yassine Cherif <cherif.esir@gmail.com>
2015-05-18 17:15:06 +0200

    Apropos: remove unused imports

--- app/src/main/java/com/esir/si/smarte_bike/Apropos.java
+++ app/src/main/java/com/esir/si/smarte_bike/Apropos.java
@@ -1,12 +1,7 @@
 package com.esir.si.smarte_bike;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 
 /**
  * Created by Toinette on 18/05/2015.
