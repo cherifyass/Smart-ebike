d2e696a98734be498f22a8c93ee853038e38edd6
Yassine Cherif <cherif.esir@gmail.com>
2015-05-25 00:55:06 +0200

    weather: save & restore state after rotation changes

--- app/src/main/java/com/esir/si/smarte_bike/Donnees.java
+++ app/src/main/java/com/esir/si/smarte_bike/Donnees.java
@@ -1,6 +1,7 @@
 package com.esir.si.smarte_bike;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -55,10 +56,10 @@ public class Donnees extends Fragment implements GoogleApiClient.ConnectionCallb
     private TextView hum;
     private TextView humLab;
     private ImageView condIcon;
+    private Bitmap iconData;
 
     // Location attributes
     private GoogleApiClient mGoogleApiClient;
-    private LocationRequest mLocationRequest;
     private boolean mWritten = false;
 
     @Override
@@ -66,6 +67,7 @@ public class Donnees extends Fragment implements GoogleApiClient.ConnectionCallb
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.donnees, container, false);
+
         //On récupère le bouton pour cacher/afficher le menu
         boutonStat = (Button) view.findViewById(R.id.boutonStat);
         boutonMeteo = (Button) view.findViewById(R.id.boutonMeteo);
@@ -75,9 +77,9 @@ public class Donnees extends Fragment implements GoogleApiClient.ConnectionCallb
         contenuStat = (RelativeLayout) view.findViewById(R.id.contenuStat);
         contenuMeteo = (RelativeLayout) view.findViewById(R.id.contenuMeteo);
         contenuSante = (RelativeLayout) view.findViewById(R.id.contenuSante);
+
         // On récupère le layout principal
         slider = (Slider) view.findViewById(R.id.slider);
-
 
         //On rajoute un Listener sur le clic du bouton stat
         boutonStat.setOnClickListener(new View.OnClickListener() {
@@ -101,7 +103,6 @@ public class Donnees extends Fragment implements GoogleApiClient.ConnectionCallb
             @Override
             public void onClick(View vue) {
                 slider.setToHide(contenuMeteo);
-
                 if (slider.toggle()) {
                     boutonMeteo.setText("- Ma météo");
                 } else {
@@ -130,40 +131,50 @@ public class Donnees extends Fragment implements GoogleApiClient.ConnectionCallb
                 .addApi(LocationServices.API)
                 .build();
 
+        if (savedInstanceState != null) {
+            restoreSavedState(view, savedInstanceState);
+            return view;
+        }
+
         /* Widget météo */
-        final ConnectivityManager cm = (ConnectivityManager) this.getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        final ConnectivityManager cm = (ConnectivityManager) this.getActivity()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
 
         // initialisation des views
-        temp = (TextView) view.findViewById(R.id.temp);
-        condIcon = (ImageView) view.findViewById(R.id.condIcon);
-        cityText = (TextView) view.findViewById(R.id.cityText);
+        initWeatherViews(view);
 
         // check if device is connected to a network
         NetworkInfo ni = cm.getActiveNetworkInfo();
-        if (ni == null)
+        if (ni == null) {
             cityText.setText("Impossible de se connecter à internet !\n " +
                     "Veuillez vérifier votre connexion !");
-        else
+        } else {
             cityText.setText("Chargement de météo ...");
+        }
 
+        return view;
+    }
+
+    public void initWeatherViews(View view) {
+        temp = (TextView) view.findViewById(R.id.temp);
+        condIcon = (ImageView) view.findViewById(R.id.condIcon);
+        cityText = (TextView) view.findViewById(R.id.cityText);
         condDescr = (TextView) view.findViewById(R.id.condDescr);
         hum = (TextView) view.findViewById(R.id.hum);
         humLab = (TextView) view.findViewById(R.id.humLab);
         windSpeed = (TextView) view.findViewById(R.id.windSpeed);
         windLab = (TextView) view.findViewById(R.id.windLab);
-
-        return view;
     }
 
     @Override
-    public void onStart(){
+    public void onStart() {
         super.onStart();
         // connect the client
         mGoogleApiClient.connect();
     }
 
     @Override
-    public void onStop(){
+    public void onStop() {
         // Disconnecting the client invalidates it.
         mGoogleApiClient.disconnect();
         super.onStop();
@@ -171,14 +182,15 @@ public class Donnees extends Fragment implements GoogleApiClient.ConnectionCallb
 
     @Override
     public void onConnected(Bundle bundle) {
-        Log.i(TAG, "Location services connected.");
 
-        mLocationRequest = LocationRequest.create();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(1000); // Update location every second
+        Log.i(TAG, "Location services connected");
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(3000); // Update location every second
 
         LocationServices.FusedLocationApi.requestLocationUpdates(
-                mGoogleApiClient, mLocationRequest, this);
+                mGoogleApiClient, locationRequest, this);
     }
 
     @Override
@@ -198,7 +210,7 @@ public class Donnees extends Fragment implements GoogleApiClient.ConnectionCallb
         // check if device is connected to a network
         NetworkInfo ni = cm.getActiveNetworkInfo();
         if (ni == null) {
-            if(!mWritten) {
+            if (!mWritten) {
                 Log.i(TAG, "No internet connection");
                 mWritten = true;
             }
@@ -212,8 +224,7 @@ public class Donnees extends Fragment implements GoogleApiClient.ConnectionCallb
             hum.setText("");
             humLab.setText("");
             condIcon.setImageDrawable(null);
-        }
-        else {
+        } else {
             JSONWeatherTask task = new JSONWeatherTask();
             task.execute(String.valueOf(location.getLatitude()), String.valueOf(location.getLongitude()));
         }
@@ -225,14 +236,14 @@ public class Donnees extends Fragment implements GoogleApiClient.ConnectionCallb
         protected Weather doInBackground(String... params) {
             Weather weather = new Weather();
 
-            String data = null;
+            String data;
             data = ((new WeatherHttpClient()).getWeatherData(params[0], params[1]));
             if (data == null)
                 return null;
 
             try {
                 weather = JSONWeatherParser.getWeather(data);
-                System.out.println("Weather [" + weather + "]");
+                Log.i(TAG, "Weather [" + weather + "]");
                 // Let's retrieve the icon
                 weather.iconData = ((new WeatherHttpClient()).getImage(weather.currentCondition.getIcon()));
 
@@ -246,10 +257,11 @@ public class Donnees extends Fragment implements GoogleApiClient.ConnectionCallb
         protected void onPostExecute(Weather weather) {
             super.onPostExecute(weather);
 
-            if(weather == null)
+            if (weather == null)
                 return;
 
-            if (weather.iconData != null ) {
+            if (weather.iconData != null) {
+                iconData = weather.iconData;
                 condIcon.setImageBitmap(weather.iconData);
                 condIcon.setAdjustViewBounds(true);
             }
@@ -258,7 +270,7 @@ public class Donnees extends Fragment implements GoogleApiClient.ConnectionCallb
 
             double celsius = weather.temperature.getTemp();
             DecimalFormat df = new DecimalFormat("0");
-            temp.setText("" + df.format(celsius)+"°");
+            temp.setText("" + df.format(celsius) + "°");
 
             condDescr.setText(weather.currentCondition.getDescr());
 
@@ -271,5 +283,35 @@ public class Donnees extends Fragment implements GoogleApiClient.ConnectionCallb
             df = new DecimalFormat("0.#");
             windSpeed.setText("" + df.format(kph) + " km/h");
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString("boutonMeteo", boutonMeteo.getText().toString());
+        savedInstanceState.putParcelable("iconData", iconData);
+        savedInstanceState.putString("temp", temp.getText().toString());
+        savedInstanceState.putString("cityText", cityText.getText().toString());
+        savedInstanceState.putString("condDescr", condDescr.getText().toString());
+        savedInstanceState.putString("hum", hum.getText().toString());
+        savedInstanceState.putString("humLab", humLab.getText().toString());
+        savedInstanceState.putString("windSpeed", windSpeed.getText().toString());
+        savedInstanceState.putString("windLab", windLab.getText().toString());
+    }
+
+    public void restoreSavedState(View view, Bundle savedInstanceState) {
+        initWeatherViews(view);
+        if(savedInstanceState.getString("boutonMeteo").equals("- Ma météo")) {
+            boutonMeteo.performClick();
+        }
+        condIcon.setImageBitmap((Bitmap) savedInstanceState.getParcelable("iconData"));
+        condIcon.setAdjustViewBounds(true);
+        temp.setText(savedInstanceState.getString("temp"));
+        cityText.setText(savedInstanceState.getString("cityText"));
+        condDescr.setText(savedInstanceState.getString("condDescr"));
+        hum.setText(savedInstanceState.getString("hum"));
+        humLab.setText(savedInstanceState.getString("humLab"));
+        windSpeed.setText(savedInstanceState.getString("windSpeed"));
+        windLab.setText(savedInstanceState.getString("windLab"));
     }
 }
