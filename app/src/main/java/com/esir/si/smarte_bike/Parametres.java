d9ec1a2f7c71660585dd128c63bcf8820107b7d9
loann_neveu <neveu.loann@gmail.com>
2015-05-25 17:20:59 +0200

    Ajout d'un indicateur de la suppression du fichier

--- app/src/main/java/com/esir/si/smarte_bike/Parametres.java
+++ app/src/main/java/com/esir/si/smarte_bike/Parametres.java
@@ -3,6 +3,7 @@ package com.esir.si.smarte_bike;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.esir.si.smarte_bike.json.JsonUtil;
 
@@ -19,6 +20,8 @@ public class Parametres extends ActionBarActivity{
 
     public void removeHistory(View view){
         JsonUtil.removeHistory(this);
+        Toast.makeText(this, "Fichier supprimé !", Toast.LENGTH_SHORT).show();
+
     }
 
 }
